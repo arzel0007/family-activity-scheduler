007dbfce7731d0da4a81bbb33e7f6ee16a05814f
Arjay Resurreccion <arjayresurreccion@Arjays-MacBook-Air.local>
2026-05-21 20:44:47 +0800

    clean rebuild

--- docs/dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/family-activity-scheduler/sw.js', { scope: '/family-activity-scheduler/' })})}
